06cd2b2b4dab1ed28ea890fa0a66aed6bab58942
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2024-11-21 14:00:46 -0300

    feat: subject entity and dtos

--- src/subjects/dto/create-subject.dto.ts
+++ src/subjects/dto/create-subject.dto.ts
@@ -1,1 +1,45 @@
-export class CreateSubjectDto {}
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateSubjectDto {
+  @ApiProperty({
+    example: 'Mathematics',
+    description: `The name of the subject.`,
+    required: true,
+  })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(35)
+  name: string;
+
+  @ApiProperty({
+    example: 'Mathematics is the study of numbers, shapes, and patterns.',
+    description: `The description of the subject.`,
+    required: true,
+  })
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    example: 100000,
+    description: `The total number of seconds dedicated to the subject.`,
+    required: false,
+  })
+  @IsOptional()
+  @IsNumber()
+  timeSpent?: number;
+
+  @ApiProperty({
+    example: '#FF0000',
+    description: `The color of the subject.`,
+    required: true,
+  })
+  @IsString()
+  color: string;
+}
--- src/subjects/entities/subject.entity.ts
+++ src/subjects/entities/subject.entity.ts
@@ -1,1 +1,14 @@
-export class Subject {}
+import { Prisma } from '@prisma/client';
+
+export class Subject implements Prisma.SubjectsUncheckedCreateInput {
+  id?: string;
+  userId: string;
+  name: string;
+  description: string;
+  timeSpent?: number;
+  color: string;
+  createdAt?: string | Date;
+  Topics?: Prisma.TopicsUncheckedCreateNestedManyWithoutSubjectsInput;
+  Activities?: Prisma.ActivitiesUncheckedCreateNestedManyWithoutSubjectsInput;
+  PlanSubjects?: Prisma.PlanSubjectsUncheckedCreateNestedManyWithoutSubjectsInput;
+}
